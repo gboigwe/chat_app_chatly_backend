a399095934ca105a74a05a52da8bdb2eb160f9f0
gboigwe <geakande@gmail.com>
2024-07-13 06:07:35 +0100

    Updated the route url

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    origin: "https://chatliapp.netlify.app",
     credentials: true,
   },
 });
